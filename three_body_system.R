b15a0c835efc471bc632bd2801399e43e6be1968
watermouth <sminakuchi@gmail.com>
2013-12-23 22:32:40 +0900

    関数の移動とsimulate.three.body.systemの追加

--- three_body_system.R
+++ three_body_system.R
@@ -10,43 +10,24 @@ initialize <- function(){
   c(1,1,1)
 }
 
-get.selector <- function(degreeoffreedom, type){
-  counter <- 0
-  #degreeoffreedom.add1 <- 1 + degreeoffreedom
-  candidates <- seq(1:degreeoffreedom)
-  f.sequential <- function() {
-    if (counter %% degreeoffreedom == 0){
-      counter <<- 1
-    } else {
-      counter <<- counter + 1
-    }
-    counter
-  }
-  f.random <- function() {sample(x=candidates,size=1,replace=T)}
-  f <- switch(type,
-              "sequential" = f.sequential,
-              "random" = f.random
+simulate.three.body.system <- function(iteration.number=100,parameter=0.2, selector.type="sequential"){
+  # 初期化
+  x <- initialize()
+  # Updaterの取得
+  selector <- get.selector(degreeoffreedom=length(x), type=selector.type)
+  updater  <- get.updater(updater.type="Metropolis"
+                          ,variable.selector=selector
+                          ,fun.prob=function(x){
+                            get.prob(x=x,para=parameter)
+                          }
   )
-  f
-}
-
-updater.metropolis <- function(varable.selector, fun.prob, candidate.sampler){
-  index <- variable.selector()
-  f <- function(x){
-    p.this <- fun.prob(x)
-    x[index] <- candidate.sampler(x[index])
-    p.candidate <- fun.prob(x)
-    ratio <- p.candidate / p.this
-    
+  # LOOP
+  v <- matrix(nrow=iteration.number + 1, ncol=length(x), byrow=T)
+  v[1,] <- x
+  for (n in seq(1:iteration.number)){
+    v[n+1,] <- updater(v[n,])
   }
-}
-
-get.updater <- function(updater.type="Metropolis", degree.of.freedom, selector.type="sequential", fun.prob) {
-  variable.selector <- get.selector(degreeoffreedom=degree.of.freedom,type=selector.type)
-  f <- switch(updater.type,
-              "Metropolis" = updater.metropolis(variable.selector, fun.prob)
-  )
-  f
+  v
 }
 
 # Initialize
---
+++ updater.R
@@ -0,0 +1,52 @@
+#' 
+#' updater(x)
+get.updater <- function(updater.type="Metropolis", variable.selector, fun.prob) {
+  f <- switch(updater.type,
+              "Metropolis" = updater.metropolis(variable.selector=variable.selector
+                                                ,fun.prob=fun.prob
+                                                ,candidate.sampler=candidate.sampler.ising)
+  )
+  f
+}
+
+get.selector <- function(degreeoffreedom, type){
+  counter <- 0
+  candidates <- seq(1:degreeoffreedom)
+  f.sequential <- function() {
+    if (counter %% degreeoffreedom == 0){
+      counter <<- 1
+    } else {
+      counter <<- counter + 1
+    }
+    counter
+  }
+  f.random <- function() {sample(x=candidates,size=1,replace=T)}
+  f <- switch(type,
+              "sequential" = f.sequential,
+              "random" = f.random
+  )
+  f
+}
+
+updater.metropolis <- function(variable.selector, fun.prob, candidate.sampler){
+  index <- variable.selector()
+  f <- function(x){
+    x.old <- x
+    p.this <- fun.prob(x)
+    x[index] <- candidate.sampler(x[index])
+    p.candidate <- fun.prob(x)
+    ratio <- p.candidate / p.this
+    x <- if ( runif(n=1, min=0, max=1) < ratio ){
+      x
+    } else {
+      x.old
+    }
+    x
+  }
+  f
+}
+
+candidate.sampler.ising <- function(x){
+  -x
+}
+
