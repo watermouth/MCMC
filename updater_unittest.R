b75795ac072bdccb73569238664704a1be49dfe7
watermouth <sminakuchi@gmail.com>
2013-12-23 23:43:56 +0900

    テストコード実行部分追加, 修正

--- updater_unittest.R
+++ updater_unittest.R
@@ -20,33 +20,43 @@ test.candidate.sampler.reverse <- function(){
 }
 
 test.updater.metropolis <- function(){
-  selector <- get.selector(degreeoffreedom=1,type="sequential")
   fun.prob <- sum
   updater <- updater.metropolis(fun.prob=fun.prob,candidate.sampler=candidate.sampler.reverse)
   x <- 1
   r <- candidate.sampler.reverse(x) / x
   stopifnot( r == -1 )
-  # -1 < 0 なので必ず採用される
-  x <- updater(x)
-  stopifnot( x == -1)
-  x <- updater(x)
+  # -1 < 0 なので必ず棄却される（本当は確率ならありえないが）
+  x <- updater(x,index=1)
+  stopifnot( x == 1)
+  x <- updater(x,index=1)
   stopifnot( x == 1)
 }
 
 test.updater.metropolis.2 <- function(){
-  selector <- get.selector(degreeoffreedom=1,type="sequential")
-  fun.prob <- function(x){sum(x)} 
+  fun.prob <- function(x){x[2]} 
   updater <- updater.metropolis(fun.prob=fun.prob,candidate.sampler=candidate.sampler.reverse)
+  # 1回目
   x <- c(1,1)
-  r <- 2 / (2) 
+  y <- c(-1,1) # reversed
+  r <- 1 / 1 
   stopifnot( r == 1 )
-  # 1 >= 1 なのでほぼ必ず棄却される
-  x <- updater(x)
-  stopifnot( sum(x) == 2)
-  x <- updater(x)
-  stopifnot( sum(x) == 2)
+  z <- updater(x,index=1)
+  # runif <= 1 なので必ず採用される
+  stopifnot(z[1] == y[1])
+  stopifnot(z[2] == y[2])
+  # 2回目
+  x <- c(-1,1)
+  y <- c(-1,-1) # reversed
+  r <- -1 / 1 
+  stopifnot( r == -1 )
+  # runif > -1 なので必ず棄却される
+  z <- updater(x, index=2)
+  stopifnot(z[1] == x[1])
+  stopifnot(z[2] == x[2])
 }
 
 
 test.get.selector()
 test.candidate.sampler.reverse()
+test.updater.metropolis()
+test.updater.metropolis.2()
