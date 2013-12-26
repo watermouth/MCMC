b3460d77111a8508a90847873585681041e8d19d
watermouth <sminakuchi@gmail.com>
2013-12-26 09:43:36 +0900

    statistics.Rを使うように変更＆指定したmcsでの結果をプロットできるようにした。

--- three_body_system.R
+++ three_body_system.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 source("updater.R")
 source("updater_unittest.R")
+source("statistics.R")
 get.prob <- function(x,para){
   theta <- para[1]
   # 2体相互作用
@@ -32,11 +33,33 @@ simulate.three.body.system <- function(iteration.number=100,parameter=0.2, selec
   v
 }
 
-iteration.number <- 10000
-burnin.num <- 2000
-x <- simulate.three.body.system(iteration.number=iteration.number,parameter=0.8, selector.type="sequential")
-# burn in
-x <- x[-(1:(burnin.num+1)),]
-matplot(x=seq(1:nrow(x))
-        , y=sapply(X=seq(1:3),FUN=function(i){cumsum(x=x[,i]) / c(1:nrow(x))})
-        ,pch=c(20,20,20))
+iteration.number <- 30000
+burnin.num <- 100
+theta <- 0.8
+x <- simulate.three.body.system(iteration.number=iteration.number,parameter=theta, selector.type="random")
+out.x <- seq(from=burnin.num, to=iteration.number - burnin.num, by=(ncol(x) * burnin.num))
+out.y <- stat.fun(
+                quantity.fun=function(x){x[1]}
+              , samples=x
+              , out.steps=out.x
+              , burnin=burnin.num)
+mcs <- out.x / ncol(x)
+plot(x=mcs, y=out.y, type="l", ylim=c(-1,1))
+analytic.y <- 0
+lines(x=mcs, y=rep(x=analytic.y,length(mcs)), col="red")
+title(main="expectation of x[1]")
+
+out.y <- stat.fun(
+                quantity.fun=function(x){x[1]*x[2]}
+              , samples=x
+              , out.steps=out.x
+              , burnin=burnin.num)
+plot(x=mcs, y=out.y, type="l", ylim=c(-1,1))
+title(main="expectation of x[1]*x[2]")
+analytic.y <- ((2*exp(3*theta) - 2*exp(-theta)) / (2*exp(3*theta) + 6*exp(-theta)))
+lines(x=mcs, y=rep(x=analytic.y, length(mcs)), col="red")
+# # burn in
+# x <- out.x[-(1:(burnin.num+1)),]
+# matplot(x=seq(1:nrow(x))
+#         , y=sapply(X=seq(1:3),FUN=function(i){cumsum(x=x[,i]) / c(1:nrow(x))})
+#         ,pch=c(20,20,20))
