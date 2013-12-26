a123aae55a4268ba769e32ccd256bdda7709ec5f
watermouth <sminakuchi@gmail.com>
2013-12-27 07:48:27 +0900

    row, colを求める部分の修正, テストコードの追加

--- nearest_neighbor_coupling_system.R
+++ nearest_neighbor_coupling_system.R
@@ -1,8 +1,8 @@
 get.prob.ratio <- function(x, index, x.candidate, para){
   stopifnot(length(index) == length(x.candidate))
   x.mat <- matrix(data=x, nrow=para$nrow, ncol=para$ncol, byrow=T)
-  idx.row <- index %/% para$ncol + 1
-  idx.col <- (index - (idx.row - 1)* para$nrow) %%  (para$nrow + 1)
+  idx.row <- 1 + ((index -1) %/% para$nrow)
+  idx.col <- 1 + ((index -1) %% para$ncol)
   idx.up   <- if (idx.row - 1 < 1) para$nrow else (idx.row - 1)
   idx.down <- if (idx.row + 1 > para$nrow) 1 else (idx.row + 1)
   idx.left <- if (idx.col - 1 < 1) para$ncol else (idx.col - 1)
@@ -16,6 +16,7 @@ get.prob.ratio <- function(x, index, x.candidate, para){
 }
 
 test.get.prob.ratio <- function(){
+  # (2,2)を更新
   #   -1        -1
   #  1 1 1 ->  1-1 1
   #    1         1
@@ -25,6 +26,42 @@ test.get.prob.ratio <- function(){
   para <- list(theta=0.1, ncol=3, nrow=3)
   actual <- get.prob.ratio(x=x,index=index,x.candidate=x.candidate,para=para)
   expected <- exp(para$theta * (-1 -1) * (-1 + 1 + 1 + 1))
+  stopifnot(expected == actual)
+  
+  # (1,2)を更新
+  #   -1         1
+  #  1 1 1 ->  1 1 1
+  #    1         1
+  x <- c(1,-1,1, 1,1,1, 1,1,1)
+  index <- 2
+  x.candidate <- 1
+  para <- list(theta=0.1, ncol=3, nrow=3)
+  actual <- get.prob.ratio(x=x,index=index,x.candidate=x.candidate,para=para)
+  expected <- exp(para$theta * (1 -(-1)) * (1 + 1 + 1 + 1))
+  stopifnot(expected == actual)
+  
+  # (1,1)を更新
+  #  1-1      -1-1
+  #  1 1 1 ->  1 1 1
+  #    1         1
+  x <- c(1,-1,1, 1,1,1, 1,1,1)
+  index <- 1
+  x.candidate <- -1
+  para <- list(theta=0.1, ncol=3, nrow=3)
+  actual <- get.prob.ratio(x=x,index=index,x.candidate=x.candidate,para=para)
+  expected <- exp(para$theta * (-1 -(1)) * (1 - 1 + 1 + 1))
+  stopifnot(expected == actual)
+  
+  # (3,3)を更新
+  #  1-1      -1-1
+  #  1 1 1 ->  1 1 1
+  #    1-1       1 1
+  x <- c(1,-1,1, 1,1,1, 1,1,-1)
+  index <- 9
+  x.candidate <- 1
+  para <- list(theta=0.1, ncol=3, nrow=3)
+  actual <- get.prob.ratio(x=x,index=index,x.candidate=x.candidate,para=para)
+  expected <- exp(para$theta * (1 -(-1)) * (1 + 1 + 1 + 1))
   stopifnot(expected == actual)
 }
 
