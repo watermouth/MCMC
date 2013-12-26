675a185e45b7f4de80f08e575600c9c2b37290ea
watermouth <sminakuchi@gmail.com>
2013-12-27 00:12:38 +0900

    2D最近接Ising model の追加

---
+++ nearest_neighbor_coupling_system.R
@@ -0,0 +1,31 @@
+get.prob.ratio <- function(x, index, x.candidate, para){
+  stopifnot(length(index) == length(x.candidate))
+  x.mat <- matrix(data=x, nrow=para$nrow, ncol=para$ncol, byrow=T)
+  idx.row <- index %/% para$ncol + 1
+  idx.col <- (index - (idx.row - 1)* para$nrow) %%  (para$nrow + 1)
+  idx.up   <- if (idx.row - 1 < 1) para$nrow else (idx.row - 1)
+  idx.down <- if (idx.row + 1 > para$nrow) 1 else (idx.row + 1)
+  idx.left <- if (idx.col - 1 < 1) para$ncol else (idx.col - 1)
+  idx.right<- if (idx.col + 1 > para$ncol) 1 else (idx.col + 1)
+  exp(para$theta * sum(c(x.mat[idx.up, idx.col]
+                   , x.mat[idx.down, idx.col]
+                   , x.mat[idx.row, idx.left]
+                   , x.mat[idx.row, idx.right]
+                   )
+                 ) * (x.candidate - x[index]))
+}
+
+test.get.prob.ratio <- function(){
+  #   -1        -1
+  #  1 1 1 ->  1-1 1
+  #    1         1
+  x <- c(1,-1,1, 1,1,1, 1,1,1)
+  index <- 5
+  x.candidate <- -1
+  para <- list(theta=0.1, ncol=3, nrow=3)
+  actual <- get.prob.ratio(x=x,index=index,x.candidate=x.candidate,para=para)
+  expected <- exp(para$theta * (-1 -1) * (-1 + 1 + 1 + 1))
+  stopifnot(expected == actual)
+}
+
+test.get.prob.ratio()
