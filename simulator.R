6bb7db40906cfd63a31286ebbce178c8016ddb1a
watermouth <sminakuchi@gmail.com>
2013-12-26 18:05:42 +0900

    メトロポリス法で使う確率比を解析的にまとめた式から求める機能を追加

--- simulator.R
+++ simulator.R
@@ -3,12 +3,12 @@ source("updater.R")
 source("updater_unittest.R")
 source("statistics.R")
 
-simulate <- function(initial.state, fun.prob, iteration.number=100, selector.type="sequential"){
+simulate <- function(initial.state, fun.prob, iteration.number=100, updater.type="MetropolisRevised", selector.type="sequential"){
   # 初期化
   x <- initial.state 
   # Updaterの取得
   selector <- get.selector(degreeoffreedom=length(x), type=selector.type)
-  updater  <- get.updater(updater.type="Metropolis", fun.prob=fun.prob)
+  updater  <- get.updater(updater.type=updater.type, fun.prob=fun.prob)
   # LOOP
   v <- matrix(nrow=iteration.number + 1, ncol=length(x), byrow=T)
   v[1,] <- x
@@ -20,15 +20,16 @@ simulate <- function(initial.state, fun.prob, iteration.number=100, selector.typ
 
 # initial.state のサイズが大きいと発散して計算できない...
 example <- function(initial.state=c(1,1,1)){
-  source("three_body_system.R")
+  source("global_two_body_coupling_system.R")
   iteration.number <- 30000
   burnin.num <- 1000
   theta <- 0.4
   x <- simulate(initial.state=initial.state
                 , fun.prob=function(x){get.prob(x=x, para=theta)}
+                , updater.type="Metropolis"
                 , iteration.number=iteration.number
                 , selector.type="random"
-                )
+  )
   out.x <- seq(from=burnin.num, to=iteration.number - burnin.num, by=(ncol(x) * burnin.num))
   prepared.stat.fun <- get.stat.fun(samples=x, out.steps=out.x, burnin=burnin.num)
   
@@ -46,5 +47,40 @@ example <- function(initial.state=c(1,1,1)){
   lines(x=mcs, y=rep(x=analytic.y, length(mcs)), col="red")
 }
 
-example(initial.state=rep(1,3))
+example2 <- function(initial.state=c(1,1,1)){
+  source("global_two_body_coupling_system.R")
+  iteration.number <- 30000
+  burnin.num <- 1000
+  theta <- 0.4
+  x <- simulate(initial.state=initial.state
+                , fun.prob=function(x, index, x.candidate){
+                  get.prob.ratio(x=x, index=index, x.candidate=x.candidate, para=theta)
+                }
+                , updater.type="MetropolisRevised"
+                , iteration.number=iteration.number
+                , selector.type="random"
+  )
+  out.x <- seq(from=burnin.num, to=iteration.number - burnin.num, by=(ncol(x) * burnin.num))
+  prepared.stat.fun <- get.stat.fun(samples=x, out.steps=out.x, burnin=burnin.num)
+  
+  out.y <- prepared.stat.fun(function(x){x[1]})
+  mcs <- out.x / ncol(x)
+  plot(x=mcs, y=out.y, type="l", ylim=c(-1,1))
+  analytic.y <- 0
+  lines(x=mcs, y=rep(x=analytic.y,length(mcs)), col="red")
+  title(main="expectation of x[1]")
+  
+  out.y <- prepared.stat.fun(function(x){x[1]*x[2]})
+  plot(x=mcs, y=out.y, type="l", ylim=c(-1,1))
+  title(main="expectation of x[1]*x[2]")
+  analytic.y <- ((2*exp(3*theta) - 2*exp(-theta)) / (2*exp(3*theta) + 6*exp(-theta)))
+  lines(x=mcs, y=rep(x=analytic.y, length(mcs)), col="red")
+}
+
+# example
+initial.state <- rep(1,3)
+set.seed(seed=1000)
+example(initial.state=initial.state)
+set.seed(seed=1000)
+example2(initial.state=initial.state)
 
--- updater.R
+++ updater.R
@@ -4,6 +4,8 @@ get.updater <- function(updater.type="Metropolis", variable.selector, fun.prob) 
   f <- switch(updater.type,
               "Metropolis" = updater.metropolis(fun.prob=fun.prob
                                                 ,candidate.sampler=candidate.sampler.reverse)
+              , "MetropolisRevised" = updater.metropolis.revised(fun.probability.ratio=fun.prob
+                                                ,candidate.sampler=candidate.sampler.reverse)
   )
   f
 }
@@ -27,6 +29,8 @@ get.selector <- function(degreeoffreedom, type){
   f
 }
 
+#' 目的分布の規格化定数を除いた部分から計算する.
+#' システムサイズが大きい場合は計算できない.
 updater.metropolis <- function(fun.prob, candidate.sampler){
   f <- function(x,index){
     x.old <- x
@@ -34,6 +38,28 @@ updater.metropolis <- function(fun.prob, candidate.sampler){
     x[index] <- candidate.sampler(x[index])
     p.candidate <- fun.prob(x)
     ratio <- p.candidate / p.this
+    if (ratio >= 1){
+      return(x)
+    }
+    x <- if ( runif(n=1, min=0, max=1) < ratio ){
+      x
+    } else {
+      x.old
+    }
+    x
+  }
+  f
+}
+
+#' 直接 比を計算する
+updater.metropolis.revised <- function(fun.probability.ratio, candidate.sampler){
+  f <- function(x,index){
+    x.old <- x
+    x[index] <- candidate.sampler(x[index])
+    ratio <- fun.probability.ratio(x.old, index, x[index])
+    if (ratio >= 1){
+      return(x)
+    }
     x <- if ( runif(n=1, min=0, max=1) < ratio ){
       x
     } else {
